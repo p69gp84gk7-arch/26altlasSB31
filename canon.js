0ea76a3aa7ae59fbbcd76e6ccfc9dba2380895fa
p69gp84gk7-arch <p69gp84gk7@privaterelay.appleid.com>
2026-03-27 10:07:08 +0100

    Update canon.js

--- canon.js
+++ canon.js
@@ -1,1 +1,17 @@
-
+const canonData =
+{
+"type": "FeatureCollection",
+"name": "baby",
+"features": [
+{ "type": "Feature", "properties": { "Name": 33 }, "geometry": { "type": "Point", "coordinates": [ 0.5766585, 42.7688748, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": 31 }, "geometry": { "type": "Point", "coordinates": [ 0.575286, 42.769135, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": 32 }, "geometry": { "type": "Point", "coordinates": [ 0.5760261, 42.7690528, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": 57 }, "geometry": { "type": "Point", "coordinates": [ 0.5772813, 42.7696649, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": 58 }, "geometry": { "type": "Point", "coordinates": [ 0.576496, 42.7694598, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": 59 }, "geometry": { "type": "Point", "coordinates": [ 0.5758098, 42.7696212, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": 60 }, "geometry": { "type": "Point", "coordinates": [ 0.5752446, 42.7696536, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": 61 }, "geometry": { "type": "Point", "coordinates": [ 0.574858, 42.7699358, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": 62 }, "geometry": { "type": "Point", "coordinates": [ 0.5745362, 42.7702367, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": 63 }, "geometry": { "type": "Point", "coordinates": [ 0.5740207, 42.770616, 0.0 ] } }
+]
+}
